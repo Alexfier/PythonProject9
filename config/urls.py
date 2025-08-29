43d035f3fc841f8baae5560c3e222f0a61d46732
oldbe <oldberg1999@gmail.com>
2025-08-29 21:57:09 +0300

    V6

--- config/urls.py
+++ config/urls.py
@@ -21,10 +21,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from config import settings
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
